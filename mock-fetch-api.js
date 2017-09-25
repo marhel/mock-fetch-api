ae24c9e8482d85ec13a2e3eef69387e4d32cbd74
Martin Hellspong <martin.hellspong@factor10.com>
2017-09-25 18:02:08 +0200

    Add proper statusTexts

--- mock-fetch-api.js
+++ mock-fetch-api.js
@@ -25,6 +25,72 @@ var extend = require('object-extend');
 var conditions = [];
 var failNextCall = false;
 
+var statusTexts = {
+   100: "Continue",
+   101: "Switching Protocols",
+   102: "Processing",
+   200: "OK",
+   201: "Created",
+   202: "Accepted",
+   203: "Non-Authoritative Information",
+   204: "No Content",
+   205: "Reset Content",
+   206: "Partial Content",
+   207: "Multi-Status",
+   208: "Already Reported",
+   226: "IM Used",
+   300: "Multiple Choices",
+   301: "Moved Permanently",
+   302: "Found",
+   303: "See Other",
+   304: "Not Modified",
+   305: "Use Proxy",
+   306: "Unused)",
+   307: "Temporary Redirect",
+   308: "Permanent Redirect",
+   400: "Bad Request",
+   401: "Unauthorized",
+   402: "Payment Required",
+   403: "Forbidden",
+   404: "Not Found",
+   405: "Method Not Allowed",
+   406: "Not Acceptable",
+   407: "Proxy Authentication Required",
+   408: "Request Timeout",
+   409: "Conflict",
+   410: "Gone",
+   411: "Length Required",
+   412: "Precondition Failed",
+   413: "Payload Too Large",
+   414: "URI Too Long",
+   415: "Unsupported Media Type",
+   416: "Range Not Satisfiable",
+   417: "Expectation Failed",
+   421: "Misdirected Request",
+   422: "Unprocessable Entity",
+   423: "Locked",
+   424: "Failed Dependency",
+   425: "Unassigned",
+   426: "Upgrade Required",
+   427: "Unassigned",
+   428: "Precondition Required",
+   429: "Too Many Requests",
+   430: "Unassigned",
+   431: "Request Header Fields Too Large",
+   451: "Unavailable For Legal Reasons",
+   500: "Internal Server Error",
+   501: "Not Implemented",
+   502: "Bad Gateway",
+   503: "Service Unavailable",
+   504: "Gateway Timeout",
+   505: "HTTP Version Not Supported",
+   506: "Variant Also Negotiates",
+   507: "Insufficient Storage",
+   508: "Loop Detected",
+   509: "Unassigned",
+   510: "Not Extended",
+   511: "Network Authentication Required"
+};
 
 global.fetch = function(uri, options) {
 
@@ -75,7 +141,7 @@ global.fetch = function(uri, options) {
 
                   return resolve(new Response("", {
                      status: 404,
-                     statusText: "Not Found"
+                     statusText: statusTexts[404]
                   }));
 
                }
@@ -83,6 +149,7 @@ global.fetch = function(uri, options) {
 
             return resolve(new Response(criteria.response.jsonData, {
                status: criteria.response.status,
+               statusText: statusTexts[criteria.response.status] || "Unassigned",
                headers: criteria.response.headers
             }));
 
@@ -91,7 +158,7 @@ global.fetch = function(uri, options) {
 
       return resolve(new Response("", {
          status: 404,
-         statusText: "Not Found"
+         statusText: statusTexts[404]
       }));
 
    });
@@ -139,6 +206,7 @@ module.exports = {
 
             condition.response = {
                status: status,
+               statusText: statusTexts[status] || "Unassigned",
                jsonData: data,
                headers: headers
             };
